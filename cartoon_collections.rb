6ec242d2c6ada5dcb9779b1a314fdc3d2738e34e
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-17 05:40:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,8 +38,8 @@ def long_planeteer_calls(array)# code an argument here
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.
+  array.detect 
 end
